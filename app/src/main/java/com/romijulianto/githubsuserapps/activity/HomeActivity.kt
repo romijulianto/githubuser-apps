4a15470f26f2d6306a83660678575ffd9207ad8a
Romi Julianto <romyjulians@gmail.com>
2022-06-28 00:41:06 +0700

    add splash and on boarding

--- app/src/main/java/com/romijulianto/githubsuserapps/activity/HomeActivity.kt
+++ app/src/main/java/com/romijulianto/githubsuserapps/activity/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.romijulianto.githubsuserapps
+package com.romijulianto.githubsuserapps.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.romijulianto.githubsuserapps.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
